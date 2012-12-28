7eeaf3168d0d49af3ac484ff5aa3a8bad126b360
Josh Adams <josh@isotope11.com>
2012-12-28 15:37:42 -0600

    More minor tweaks, not good yet, matches too little for strings.

--- test/lib/wikitop/wikilink_test.rb
+++ test/lib/wikitop/wikilink_test.rb
@@ -2,6 +2,12 @@ require_relative '../../test_helper'
 include Wikitop
 
 describe Wikitop do
+  it "parses strings" do
+    tree = Parser.new.parse("foo")
+    tree.must_be_instance_of Phrase
+    tree.elements[0].must_be_instance_of Wikitop::String
+  end
+
   it "parses wiki links" do
     tree = Parser.new.parse("[[link]]")
     tree.must_be_instance_of Phrase
@@ -10,7 +16,7 @@ describe Wikitop do
   end
 
   it "parses text embedding wiki links" do
-    tree = Parser.new.parse("foo [[link]]")
+    tree = Parser.new.parse("*foo* [[link]]")
     tree.must_be_instance_of Phrase
     tree.elements[0].must_be_instance_of Wikitop::String
     tree.elements[1].must_be_instance_of WikiLink
