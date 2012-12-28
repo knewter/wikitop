45dc0770c44e5d89440b33a3bce18b442fd0d0e6
Josh Adams <josh@isotope11.com>
2012-12-28 13:35:04 -0600

    Add another functional test, spanning multiple lines.

--- test/functional/outputting_wiki_text_test.rb
+++ test/functional/outputting_wiki_text_test.rb
@@ -6,4 +6,9 @@ describe Wikitop do
     output = Parser.new.parse("This is a [[link]] to another wiki page").to_html
     output.must_equal "This is a <a href='/link'>link</a> to another wiki page"
   end
+
+  it "outputs useful text with link, spanning multiple lines properly" do
+    output = Parser.new.parse("This is a [[link]] to another \nwiki page").to_html
+    output.must_equal "This is a <a href='/link'>link</a> to another \nwiki page"
+  end
 end
