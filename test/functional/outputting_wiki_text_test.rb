09c1a11d78a6f7c9a75013cc9f51f0f1185bcdad
Josh Adams <josh@isotope11.com>
2012-12-28 17:35:11 -0600

    Support more keyboard chars arg

--- test/functional/outputting_wiki_text_test.rb
+++ test/functional/outputting_wiki_text_test.rb
@@ -3,8 +3,8 @@ include Wikitop
 
 describe Wikitop do
   it "outputs useful text with link" do
-    output = Parser.new.parse("This is a [[link]] to another wiki page").to_html
-    output.must_equal "This is a <a href='/link'>link</a> to another wiki page"
+    output = Parser.new.parse("This: is a [[link]] to another wiki page").to_html
+    output.must_equal "This: is a <a href='/link'>link</a> to another wiki page"
   end
 
   it "outputs useful text with link, spanning multiple lines properly" do
